73dcec1c91861e924b23be451d47e55ee6674296
crackh <133303921+crackh@users.noreply.github.com>
2025-10-15 16:49:50 +0300

    Create next-sitemap.config.js

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://jeremiahkidsfoundation.org', // your live site URL
+  generateRobotsTxt: true, // also create robots.txt
+  sitemapSize: 7000,
+};
